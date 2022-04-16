f238f3a88ac9cd8c8f9a236993ebbadd8cdbb800
kakorotto <paulaatiya@gmail.com>
2022-04-17 01:40:28 +0200

    modifing blades

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,21 +1,30 @@
-@extends('layout.app')
+@extends('layouts.app')
 
-@section('title') Create @endsection
+@section('title')Create @endsection
 
 @section('content')
-      <form method="POST" action="/posts/store">
-        @csrf
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Title</label>
-            <input type="text" name="title" class="form-control" id="exampleFormControlInput1">
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Posted by</label>
-            <input type="text" name="posted_by" class="form-control" id="exampleFormControlInput1">
-          </div>
+<form method="POST" action="{{route('posts.store')}}">
+    @csrf
+    <div class="my-4">
+        <label for="exampleFormControlInput1" class="form-label">Title</label>
+        <input name="title" type="text" class="form-control" id="exampleFormControlInput1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+        <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
 
-          <div class="mb-3">
-                <button type="submit" class="btn btn-success">Create Post</button>
-          </div>
-        </form>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Post Creator</label>
+        <select name="post_creator" class="form-control">
+            @foreach($users as $user)
+            <option value="{{$user->id}}">{{$user->name}}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-success">Create Post</button>
+    </div>
+</form>
 @endsection
